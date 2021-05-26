d8609b9473ffb6fb9bdd2e9d984f6e32f8e00335
tony <tony92151@gmail.com>
2021-05-26 20:27:05 +0800

    update femnist config

--- femnist_train.sh
+++ femnist_train.sh
@@ -1,29 +1,31 @@
 memory_checkpoint=./tmp/test_0
 
-# small_dgc_cr1_iid
-# python3 train.py --config ./configs/femnist/config_0.ini --output ./save/femnist_test
-# sleep 30
-# small_dgc_cr1_niid
+# femnist_baseline
+python3 train.py --config ./configs/femnist/config_0.ini --output ./save/femnist_test --pool auto --gpu 0
+sleep 30
+# femnist_small_dgc_cr1.0_niid
 python3 train.py --config ./configs/femnist/config_1.ini --output ./save/femnist_test --pool auto --gpu 0
 sleep 30
-# small_dgc_cr0.8_niid
+# femnist_small_gfdgc_cr1.0_fr0.5_niid
 python3 train.py --config ./configs/femnist/config_2.ini --output ./save/femnist_test --pool auto --gpu 0
 sleep 30
-# small_dgc_cr0.6_niid
+# femnist_small_gfdgc_cr1.0_fr0.75_niid
 python3 train.py --config ./configs/femnist/config_3.ini --output ./save/femnist_test --pool auto --gpu 0
 sleep 30
-# small_dgc_cr0.4_niid
+# femnist_small_dgc_cr_scale_fr0.5_niid
 python3 train.py --config ./configs/femnist/config_4.ini --output ./save/femnist_test --pool auto --gpu 0
 sleep 30
-# small_gfdgc_cr1_fu0.5_niid
+# femnist_small_dgc_cr_scale_fr0.75_niid
 python3 train.py --config ./configs/femnist/config_5.ini --output ./save/femnist_test --pool auto --gpu 0
 sleep 30
-# small_gfdgc_cr0.8_fu0.5_niid
-python3 train.py --config ./configs/femnist/config_6.ini --output ./save/femnist_test --pool auto --gpu 0
-sleep 30
-# small_gfdgc_cr0.6_fu0.5_niid
-python3 train.py --config ./configs/femnist/config_7.ini --output ./save/femnist_test --pool auto --gpu 0
-sleep 30
-# small_gfdgc_cr0.4_fu0.5_niid
-python3 train.py --config ./configs/femnist/config_8.ini --output ./save/femnist_test --pool auto --gpu 0
-sleep 30
+
+
+## small_gfdgc_cr0.8_fu0.5_niid
+#python3 train.py --config ./configs/femnist/config_6.ini --output ./save/femnist_test --pool auto --gpu 0
+#sleep 30
+## small_gfdgc_cr0.6_fu0.5_niid
+#python3 train.py --config ./configs/femnist/config_7.ini --output ./save/femnist_test --pool auto --gpu 0
+#sleep 30
+## small_gfdgc_cr0.4_fu0.5_niid
+#python3 train.py --config ./configs/femnist/config_8.ini --output ./save/femnist_test --pool auto --gpu 0
+#sleep 30
