c609bc93e6bff584e06c0b76f235be95ec772712
tony <tony92151@gmail.com>
2021-07-10 17:21:13 +0800

    update shakespeare config and model

--- utils/shakespeare_model/stacked_lstm.py
+++ utils/shakespeare_model/stacked_lstm.py
@@ -4,9 +4,30 @@ import torch
 
 
 # (0.0003, 80, 80, 256), # lr, seq_len, num_classes, num_hidden
+# class LSTM_shakespeare(torch.nn.Module):
+#     def __init__(self, n_vocab=80, embedding_dim=80, hidden_dim_1=256, hidden_dim_2=256,
+#                  nb_layers_1=4, nb_layers_2=1, dropout=0.2):
+#         super(LSTM_shakespeare, self).__init__()
+
+#         self.embedding_dim = embedding_dim
+#         self.hidden_dim_1 = hidden_dim_1
+#         self.hidden_dim_2 = hidden_dim_2
+
+#         self.embeddings = torch.nn.Embedding(n_vocab, embedding_dim)
+#         self.lstm_1 = torch.nn.LSTM(embedding_dim, hidden_dim_1, nb_layers_1, dropout=dropout)
+#         self.lstm_2 = torch.nn.LSTM(hidden_dim_1, hidden_dim_2, nb_layers_2)
+#         self.hidden2out = torch.nn.Linear(hidden_dim_2, n_vocab)
+
+#     def forward(self, seq_in):
+#         embeddings = self.embeddings(seq_in.t())
+#         lstm_out, _ = self.lstm_1(embeddings)
+#         lstm_out, _ = self.lstm_2(lstm_out)
+#         ht = lstm_out[-1]
+#         out = self.hidden2out(ht)
+#         return out
+
 class LSTM_shakespeare(torch.nn.Module):
-    def __init__(self, n_vocab=80, embedding_dim=3, hidden_dim_1=80, hidden_dim_2=256, nb_layers_1=2,
-                 nb_layers_2=1):
+    def __init__(self, n_vocab=80, embedding_dim=3, hidden_dim_1=256, hidden_dim_2=256, nb_layers_1=1, nb_layers_2=1):
         super(LSTM_shakespeare, self).__init__()
 
         self.embedding_dim = embedding_dim
@@ -14,19 +35,14 @@ class LSTM_shakespeare(torch.nn.Module):
         self.hidden_dim_2 = hidden_dim_2
 
         self.embeddings = torch.nn.Embedding(n_vocab, embedding_dim)
-        self.lstm_1 = torch.nn.LSTM(embedding_dim, hidden_dim_1, nb_layers_1, dropout=0.5)
+        self.lstm_1 = torch.nn.LSTM(embedding_dim, hidden_dim_1, nb_layers_1)
         self.lstm_2 = torch.nn.LSTM(hidden_dim_1, hidden_dim_2, nb_layers_2)
         self.hidden2out = torch.nn.Linear(hidden_dim_2, n_vocab)
 
-    def forward(self, seq_in, statas: dict = None):
-        if statas is None:
-            raise
+    def forward(self, seq_in):
         embeddings = self.embeddings(seq_in.t())
-        lstm_out, state_1 = self.lstm_1(embeddings)
-        lstm_out, state_2 = self.lstm_2(lstm_out)
+        lstm_out, _ = self.lstm_1(embeddings)
+        lstm_out, _ = self.lstm_2(lstm_out)
         ht = lstm_out[-1]
         out = self.hidden2out(ht)
-        return out, (state_1, state_2)
-
-    def zero_state(self, batch_size):
-        return (torch.zeros(1, batch_size, self.embedding_dim), torch.zeros(1, batch_size, self.hidden_dim_1))
+        return out
