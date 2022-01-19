6c85e9322a9753ca6be1b2cead91abe59a08e730
tony92151 <tony92151@gmail.com>
2022-01-19 09:56:47 +0800

    update reme and bash script

--- train_sha.sh
+++ train_sha.sh
@@ -10,11 +10,6 @@ then
   gpu="0"
 fi
 
-if [ -z "name_prefix" ]
-then
-  name_prefix=""
-fi
-
 if [ -z "tbpath" ]
 then
   tbpath="sha_repo_exp"
@@ -25,85 +20,94 @@ then
   compress_method="DGC"
 fi
 
-
+if [ -z "seed" ]
+then
+  seed=123
+fi
 
 
 echo "Python interpreter: $pyenv"
 echo "Torch version : $($pyenv -c 'import torch; print(torch.__version__)')"
 echo "GPU : $gpu"
 echo "tbpath : $tbpath"
+echo "seed : $seed"
 
-#######################################################
-# environment variable overwrite
-# This will overwrite parameter in .ini file while initializing the dataloader
+python3 train.py \
+--config ./configs/shakespeare/$compress_method/config_1.ini \
+--tensorboard_path ./"$tbpath"/sha_1l_"$compress_method"_cr1.0 \
+--output ./"$tbpath"/sha_1l_"$compress_method"_cr1.0 \
+--gpu $gpu \
+--pool 5 \
+--seed $seed
 
-#export compress_method="DGC"
-#######################################################
+# python3 train.py \
+# --config ./configs/shakespeare/$compress_method/config_2.ini \
+# --tensorboard_path ./"$tbpath"/sha_1l_"$compress_method"_cr0.2 \
+# --output ./"$tbpath"/sha_1l_"$compress_method"_cr0.2 \
+# --gpu $gpu \
+# --pool 5 \
+# --seed $seed
 
-## fedavg iid
-#$pyenv train.py --config ./configs/cifar10/config_fedavg_iid.ini --output ./save/cifar10_final --pool 5 --gpu $gpu --name_prefix $name_prefix
-#sleep 10
-#
-## fedavg niid
-#$pyenv train.py --config ./configs/cifar10/config_fedavg_niid.ini --output ./save/cifar10_final --pool 5 --gpu $gpu --name_prefix $name_prefix
-#sleep 10
-
-## dgc iid
-#$pyenv train.py --config ./configs/cifar10/config_dgc_iid.ini --output ./save/cifar10_final --pool 5 --gpu $gpu --name_prefix $name_prefix
-#sleep 10
-#
-## dgc niid
-#$pyenv train.py --config ./configs/cifar10/config_dgc_niid.ini --output ./save/cifar10_final --pool 5 --gpu $gpu --name_prefix $name_prefix
-#sleep 10
-
-############################################################################################################
-############################################################################################################
-
-
-# # compress ratio 0.1
-# $pyenv train_1.py \
-# --config ./configs/shakespeare/$compress_method/config_1.ini \
-# --tensorboard_path ./"$tbpath"/sha_1l_"$compress_method"_cr0.1 \
-# --output ./"$tbpath"/sha_1l_"$compress_method"_cr0.1 \
-# --pool 8 \
-# --seed 123 \
-# --gpu $gpu
-
-# compress ratio 0.3
-$pyenv train_1.py \
+python3 train.py \
 --config ./configs/shakespeare/$compress_method/config_3.ini \
 --tensorboard_path ./"$tbpath"/sha_1l_"$compress_method"_cr0.3 \
 --output ./"$tbpath"/sha_1l_"$compress_method"_cr0.3 \
---pool 8 \
---seed 123 \
---gpu $gpu
+--gpu $gpu \
+--pool 5 \
+--seed $seed
 
-# # compress ratio 0.5
-# $pyenv train_1.py \
-# --config ./configs/shakespeare/$compress_method/config_5.ini \
-# --tensorboard_path ./"$tbpath"/sha_1l_"$compress_method"_cr0.5 \
-# --output ./"$tbpath"/sha_1l_"$compress_method"_cr0.5 \
-# --pool 8 \
-# --seed 123 \
-# --gpu $gpu
+# python3 train.py \
+# --config ./configs/shakespeare/$compress_method/config_4.ini \
+# --tensorboard_path ./"$tbpath"/sha_1l_"$compress_method"_cr0.4 \
+# --output ./"$tbpath"/sha_1l_"$compress_method"_cr0.4 \
+# --gpu $gpu \
+# --pool 5 \
+# --seed $seed
 
-# compress ratio 0.7
-$pyenv train_1.py \
+python3 train.py \
+--config ./configs/shakespeare/$compress_method/config_5.ini \
+--tensorboard_path ./"$tbpath"/sha_1l_"$compress_method"_cr0.5 \
+--output ./"$tbpath"/sha_1l_"$compress_method"_cr0.5 \
+--gpu $gpu \
+--pool 5 \
+--seed $seed
+
+# python3 train.py \
+# --config ./configs/shakespeare/$compress_method/config_6.ini \
+# --tensorboard_path ./"$tbpath"/sha_1l_"$compress_method"_cr0.6 \
+# --output ./"$tbpath"/sha_1l_"$compress_method"_cr0.6 \
+# --gpu $gpu \
+# --pool 5 \
+# --seed $seed
+
+python3 train.py \
 --config ./configs/shakespeare/$compress_method/config_7.ini \
 --tensorboard_path ./"$tbpath"/sha_1l_"$compress_method"_cr0.7 \
 --output ./"$tbpath"/sha_1l_"$compress_method"_cr0.7 \
---pool 8 \
---seed 123 \
---gpu $gpu
+--gpu $gpu \
+--pool 5 \
+--seed $seed
 
-# compress ratio 0.9
-$pyenv train_1.py \
+# python3 train.py \
+# --config ./configs/shakespeare/$compress_method/config_8.ini \
+# --tensorboard_path ./"$tbpath"/sha_1l_"$compress_method"_cr0.8 \
+# --output ./"$tbpath"/sha_1l_"$compress_method"_cr0.8 \
+# --gpu $gpu \
+# --pool 5 \
+# --seed $seed
+
+python3 train.py \
 --config ./configs/shakespeare/$compress_method/config_9.ini \
 --tensorboard_path ./"$tbpath"/sha_1l_"$compress_method"_cr0.9 \
 --output ./"$tbpath"/sha_1l_"$compress_method"_cr0.9 \
---pool 8 \
---seed 123 \
---gpu $gpu
+--gpu $gpu \
+--pool 5 \
+--seed $seed
 
-
-
+# python3 train.py \
+# --config ./configs/shakespeare/$compress_method/config_10.ini \
+# --tensorboard_path ./"$tbpath"/sha_1l_"$compress_method"_cr1.0 \
+# --output ./"$tbpath"/sha_1l_"$compress_method"_cr1.0 \
+# --gpu $gpu \
+# --pool 5 \
+# --seed $seed
